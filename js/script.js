471cac62698976f11ada22198d9a77896323b252
Lex Zavala <lex.zavala7@gmail.com>
2021-05-16 22:01:09 -0500

    started working on functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict"
+
+const card = $('.cardContainer');
+const container = $('.mainContainer');
+
+//moving animation container
+
+container.mousemove(function (e){
+    console.log(e.pageX, e.pageY);
+    let xAxis = (window.innerWidth/2 - e.pageX) / 10;
+    let yAxis = (window.innerHeight/2 - e.pageY) / 10;
+    card.css( 'transform',  `rotateY(${xAxis}deg)`, `rotateX(${yAxis}deg)`);
+
+});
